a868358c712b3f0beb7b55ed689bccad1efb75f0
StepyginSergey <stepygin.sergey@gmail.com>
2018-03-31 14:17:40 +0300

    Создание структуры проекта и главного файла приложения

---
+++ src/ru/ss/homeapp/easybackupfiles/EasyBackupFilesMain.java
@@ -0,0 +1,16 @@
+package ru.ss.homeapp.easybackupfiles;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class EasyBackupFilesMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
