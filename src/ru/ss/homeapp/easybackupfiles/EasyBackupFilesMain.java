421042b76bcafdf3162d636c6a4f7a993972654e
StepyginSergey <stepygin.sergey@gmail.com>
2018-03-31 22:12:42 +0300

    Добавлен метод копирования файла. Добавлен файл с заметками.

--- src/ru/ss/homeapp/easybackupfiles/EasyBackupFilesMain.java
+++ src/ru/ss/homeapp/easybackupfiles/EasyBackupFilesMain.java
@@ -1,5 +1,9 @@
 package ru.ss.homeapp.easybackupfiles;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -7,10 +11,44 @@ public class EasyBackupFilesMain extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		
+
+		// D:\eclipseProjects\from
+		String from = "D:/eclipseProjects/from";
+		String to = "D:/eclipseProjects/to";
+
+		getFileTree(from);
+		// copyFiles(from, to);
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	private void getFileTree(String from) {
+
+		System.out.println("start directory: " + from);
+		
+		File file = new File(from);
+		
+		if(file.isDirectory()) {
+			getFileTree(file.getPath());
+		}
+		
+	}
+
+	private void copyFiles(String from, String to) {
+		File fileSource = new File(from);
+		File fileTarget = new File(to);
+
+		if (fileTarget.exists()) {
+			fileTarget.delete();
+		}
+
+		try {
+			Files.copy(fileSource.toPath(), fileTarget.toPath());
+		} catch (IOException ex) {
+			System.out.println("copy file failed: name=" + from);
+			ex.printStackTrace();
+		}
 	}
 }
